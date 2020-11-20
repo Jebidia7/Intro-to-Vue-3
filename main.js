05faed2355abce66e03e9c48c43fc424aae2d4dc
Mike Hicks <michael.hicks@mhaas.io>
2020-11-20 12:40:01 -0600

    completed

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Boots',
+            description: "They'll walk all over you!"
+        }
+    }
+})
